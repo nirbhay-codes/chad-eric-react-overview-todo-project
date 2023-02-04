02cd9de0753557430d655399999ea28cf1e572e6
Nirbhay Lourembam <l.nirbhay@gmail.com>
2023-02-04 17:06:50 +0530

    add window confirm dialog to delete a Todo

--- src/components/TodoRowItem.js
+++ src/components/TodoRowItem.js
@@ -1,6 +1,22 @@
 function TodoRowItem(props) {
+  //   const handleClick = () => {
+  //     const confirm = window.confirm(
+  //       "Are you sure you want to delete this row now ?"
+  //     );
+  //     if (confirm) {
+  //       console.log("calling deleteTodo");
+  //       props.deleteTodo(props.rowNumber);
+  //     }
+  //   };
+
   return (
-    <tr onClick={() => props.deleteTodo(props.rowNumber)}>
+    <tr
+      onClick={() =>
+        window.confirm("Are you sure you want to delete this row?")
+          ? props.deleteTodo(props.rowNumber)
+          : null
+      }
+    >
       <th scope="row">{props.rowNumber}</th>
       <td>{props.rowDescription}</td>
       <td>{props.rowAssigned}</td>
